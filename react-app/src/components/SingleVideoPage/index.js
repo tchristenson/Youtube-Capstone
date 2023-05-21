19ef52ca3faa3193c2c8dc42759e2b77f102705f
Tommy Christenson <tchristenson21@gmail.com>
2023-05-21 12:54:26 -0500

    added sidebar videos to single video page

--- react-app/src/components/SingleVideoPage/index.js
+++ react-app/src/components/SingleVideoPage/index.js
@@ -3,6 +3,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { useParams, NavLink } from "react-router-dom";
 import { getSingleVideoThunk } from "../../store/videos";
 import { getCommentsByVideoIdThunk } from "../../store/comments";
+import { getAllVideosThunk } from "../../store/videos";
 import NewComment from "../NewComment";
 import CommentList from "../CommentList";
 import styles from './SingleVideoPage.module.css'
@@ -15,18 +16,42 @@ function SingleVideoPage() {
     useEffect(() => {
         dispatch(getSingleVideoThunk(videoId))
         dispatch(getCommentsByVideoIdThunk(videoId))
+        dispatch(getAllVideosThunk())
     }, [dispatch, videoId])
 
     const video = useSelector(state => state.videos[videoId])
     const comments = useSelector(state => state.comments)
+    const allVideos = useSelector(state => state.videos)
     const sessionUser = useSelector(state => state.session.user)
 
     if (!video) return null
 
     const commentsArr = Object.values(comments)
+    const allVideosArr = Object.values(allVideos)
+    const filteredVideos = allVideosArr.filter(currVideo => currVideo.id !== video.id)
+    console.log('filteredVideos', filteredVideos)
     // console.log('commentsArr inside SingleVideoPage', commentsArr)
     // console.log('video inside SingleVideoPage', video)
     // console.log('comments inside SingleVideoPage', comments)
+
+    const sidebarVideos = filteredVideos.map(video => (
+        <div className={styles['sidebar-video']}>
+            <div className={styles['sidebar-thumbnail']}>
+                <NavLink key={video.user.id} to={`/videos/${video.id}`}>
+                    <img src={video.thumbnail}/>
+                </NavLink>
+            </div>
+            <div className={styles['sidebar-video-details']}>
+                <NavLink key={video.id} to={`/videos/${video.id}`}>
+                    <h3 className={styles['h3link']}>{video.name}</h3>
+                </NavLink>
+                <NavLink key={video.user.id} to={`/channels/${video.user.id}`}>
+                    <h5 className={styles['h5link']}>{video.user.username}</h5>
+                </NavLink>
+            </div>
+
+        </div>
+    ))
 
     return (
         <div className={styles['container']}>
@@ -55,9 +80,8 @@ function SingleVideoPage() {
                     <CommentList video={video} commentsArr={commentsArr} sessionUser={sessionUser}/>
                 </div>
             </div>
-            <div className={styles['secondary-column']}>
-                <h1>Other videos to go in this column</h1>
-
+            <div className={styles['sidebar']}>
+                {sidebarVideos}
             </div>
         </div>
     )
