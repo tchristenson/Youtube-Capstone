bd72e1e2d397fd009c9686402dc830aa7901b97c
Tommy Christenson <tchristenson21@gmail.com>
2023-06-07 12:19:46 -0500

    playlist component in process

--- react-app/src/components/UserPlaylistsPage/index.js
+++ react-app/src/components/UserPlaylistsPage/index.js
@@ -32,28 +32,32 @@ function UserPlaylistsPage() {
     if (!playlist) return null
 
     const playlistVideos = playlist?.videos.map(video => (
-        <div key={video.id} className={styles['sidebar-video']}>
-        <div className={styles['sidebar-thumbnail']}>
-            <NavLink  to={`/videos/${video.id}`}>
-                <img className={styles['playlist-video']} src={video.thumbnail} alt="Video Thumbnail"/>
-            </NavLink>
-        </div>
-        <div className={styles['sidebar-video-details']}>
-            <NavLink to={`/videos/${video.id}`}>
-                <h5 className={styles['sidebar-video-name']}>{video.name}</h5>
-            </NavLink>
-        </div>
-    </div>
+        <NavLink key={video.id} to={`/videos/${video.id}`}>
+            <div className={styles['sidebar-video']}>
+                <div className={styles['sidebar-thumbnail']}>
+
+                        <img className={styles['playlist-video']} src={video.thumbnail} alt="Video Thumbnail"/>
+                </div>
+                <div className={styles['sidebar-video-details']}>
+                        <h5 className={styles['sidebar-video-name']}>{video.name}</h5>
+                </div>
+            </div>
+        </NavLink>
     ))
 
     return (
-
         <div className={styles["playlist-page-container"]}>
             <div className={styles["playlist-thumbnail-container"]}>
                 <img className={styles["playlist-thumbnail"]} src={playlist?.videos[0].thumbnail}/>
                 <h3>{playlist?.name}</h3>
                 <h5>{`${sessionUser.firstName} ${sessionUser.lastName}`}</h5>
-                <h6>{`${playlist?.videos.length} videos`}</h6>
+                <h6>
+                    {playlist?.videos.length === 1 ? (
+                        `${playlist.videos.length} video`
+                    ) : (
+                        `${playlist.videos.length} videos`
+                    )}
+                </h6>
             </div>
 
             <div className={styles["playlist-videos-container"]}>
