3ad063ec61e1317ed98b72c0459e62032b26a917
Tommy Christenson <tchristenson21@gmail.com>
2023-07-06 13:11:52 -0500

    css updates

--- react-app/src/components/SearchResults/index.js
+++ react-app/src/components/SearchResults/index.js
@@ -10,27 +10,26 @@ function SearchResults() {
     console.log('searchResultsArr', searchResultsArr)
 
     const videosList = searchResultsArr.map(video => (
-        <div key={video.id} className={styles['sidebar-video']}>
-        <div className={styles['sidebar-thumbnail']}>
-            <NavLink  to={`/videos/${video.id}`}>
-                <img src={video.thumbnail} alt="Video Thumbnail"/>
-            </NavLink>
+        <div key={video.id} className={styles['search-video']}>
+            <div className={styles['thumbnail']}>
+                <NavLink to={`/videos/${video.id}`}>
+                    <img src={video.thumbnail} alt="Video Thumbnail"/>
+                </NavLink>
+            </div>
+            <div className={styles['search-video-details']}>
+                <NavLink to={`/videos/${video.id}`}>
+                    <h5 className={styles['search-video-name']}>{video.name}</h5>
+                </NavLink>
+                <NavLink to={`/channels/${video.user?.id}`}>
+                    <h5 className={styles['h5link']}>{video.user?.username}</h5>
+                </NavLink>
+            </div>
         </div>
-        <div className={styles['sidebar-video-details']}>
-            <NavLink to={`/videos/${video.id}`}>
-                <h5 className={styles['sidebar-video-name']}>{video.name}</h5>
-            </NavLink>
-            <NavLink to={`/channels/${video.user?.id}`}>
-                <h5 className={styles['h5link']}>{video.user?.username}</h5>
-            </NavLink>
-        </div>
-    </div>
-
     ))
 
     return (
         <div className={styles["search-results-container"]}>
-            <h2>Your search results:</h2>
+            <h2 className={styles["header"]}>Your search results:</h2>
             <div>{videosList.length ? (
                 videosList
             ) : (
